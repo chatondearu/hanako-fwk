4b1e8c37cb2e7dc442a67586cdd9de2761f10ddf
sxmcrow <sxmcrow@iwantgeek.com>
2012-12-18 16:14:21 +0100

    rename hanako.php to Hanako.php

--- common/system/modules/epicTwitter/conf.php
+++ common/system/modules/epicTwitter/conf.php
@@ -2,7 +2,7 @@
 
     /* Define Twitter App key and token */
 
-    define('CONSUMER_KEY','RTdRjTm6pvz5giNy2nn8aw');
-    define('CONSUMER_SECRET','iFM81OPz5q5ZA6lC1Ke8Y7nR9G12VGDtGg1pfMtSFjA');
-    $oauth_token = '103003192-aY8dZ9GjYpqtAUsJguqSk7iPr6S8EBIVS9HjBTvq' ;
-    $oauth_token_secret = 'PHUvF6Szn9NAegzKMXLhIflTPiqBe57H02fO2qX5o' ;
+    define('CONSUMER_KEY','');
+    define('CONSUMER_SECRET','');
+    $oauth_token = '' ;
+    $oauth_token_secret = '' ;
